54941f1bbeb9379926fabed819b3590b8420f993
potorochinau <3dspaun@gmail.com>
2019-02-14 17:15:27 +0300

    - get current protocol, hostname, port to gen. url for OIDC redirect

--- src/containers/PrivateRouter/index.js
+++ src/containers/PrivateRouter/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from "react-router";
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-const url = 'https://api.mixapp.io/oidc/mixapp/authorize?response_type=id_token+token&client_id=5a82de9435b3820437d23cfd&redirect_uri=http://localhost:3000/authorize&scope=openid+email+profile&state=uUpgnZBBCBMnI_GLGIzCP3AZXzavFzEVC5hM6UKB_ew&nonce=UXwkyVyGj-Lw_-zEUMbySDW2A4C5G1tYA1_HKrH0-r4&display=popup'
+const getURL = (site) => {
+    return 'https://api.mixapp.io/oidc/mixapp/authorize?response_type=id_token+token&client_id=5a82de9435b3820437d23cfd&redirect_uri=' + site + '/authorize&scope=openid+email+profile&state=uUpgnZBBCBMnI_GLGIzCP3AZXzavFzEVC5hM6UKB_ew&nonce=UXwkyVyGj-Lw_-zEUMbySDW2A4C5G1tYA1_HKrH0-r4&display=popup';
+}
+
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={(props) => {
         if (!rest.app.user) {
+            let { protocol, hostname, port } = window.location;
             localStorage.setItem('redirect', rest.path);
-            window.location.href = url;
+            window.location.href = getURL(protocol + '//' + hostname + (port !== 80 ? ':' + port : 80));
             return null;
         }
         return rest.app.user ? <Component {...props} /> : null;
